b0887d61f55edf87df43dfb0b014198190fc2c3d
Edson <c1297934@interno.bb.com.br>
2024-11-09 17:53:20 -0300

    criação action

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
-
+terraform {
+  backend "s3" {
+    bucket = "fiap-fase03-tf"
+    key    = "rds/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 provider "aws" {
   region  = var.regionDefault
